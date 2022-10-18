5d1d11eeb4623c6b58da753824a061e6704b2743
chuspita <kosecki99@gmail.com>
2022-10-18 17:12:27 +0200

    sweet: Hello Xtended!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_sweet.mk
+    $(LOCAL_DIR)/xtended_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_sweet-user \
-    spark_sweet-userdebug \
-    spark_sweet-eng
+    xtended_sweet-user \
+    xtended_sweet-userdebug \
+    xtended_sweet-eng
--- xtended_sweet.mk
+++ xtended_sweet.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Spark OS  Stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+# Inherit common Xtended Stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-PRODUCT_NAME := spark_sweet
+PRODUCT_NAME := xtended_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
@@ -22,9 +22,11 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Spark OS Stuffs
+# Xtended Stuffs
 WITH_GAPPS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_BLUR := true
 TARGET_NO_PREBUILT_CAMERA := true
+XTENDED_BUILD_MAINTAINER := chuspita
+XTENDED_BUILD_TYPE := UNOFFICIAL
